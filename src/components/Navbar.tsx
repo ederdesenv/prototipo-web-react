05afbbcdbd8e783f9b22420c07fef921f8705f6b
ederdesenv <ecouto_dev@hotmail.com>
2026-06-15 23:32:30 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
           href="#"
           style={{ paddingTop: 0, paddingBottom: 0, display: 'flex', alignItems: 'center' }}
         >
-          <img src="/prototipo-web-react/imagem/logo.png" alt="OpsDrone Logo" style={{ height: '50px' }} />
+          <img src="public/imagem/logo.png" alt="OpsDrone Logo" style={{ height: '50px' }} />
           OpsDrone
         </a>
         <button
